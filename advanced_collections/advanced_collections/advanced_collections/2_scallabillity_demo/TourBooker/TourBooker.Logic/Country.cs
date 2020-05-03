48c46d4ea3712e338e1c4928135a6027df5b2f07
Naveen Kummar <r.naveenkummar@gmail.com>
2020-05-03 14:23:48 +0530

    Add operator == and !=
    Both operators levrage the previously added Equals()

--- advanced_collections/advanced_collections/advanced_collections/2_scallabillity_demo/TourBooker/TourBooker.Logic/Country.cs
+++ advanced_collections/advanced_collections/advanced_collections/2_scallabillity_demo/TourBooker/TourBooker.Logic/Country.cs
@@ -32,6 +32,23 @@ namespace Pluralsight.AdvCShColls.TourBooker.Logic
 
         public override int GetHashCode() => StringComparer.OrdinalIgnoreCase.GetHashCode(this.Value);
 
+        public static bool operator ==(CountryCode lhs, CountryCode rhs)
+        {
+            if(lhs != null)
+            {
+                return lhs.Equals(rhs);
+            }
+            else
+            {
+                return rhs == null;
+            }
+        }
+
+        public static bool operator !=(CountryCode lhs, CountryCode rhs)
+        {
+            return !(lhs == rhs);
+        }
+
     }
 
 	public class Country
