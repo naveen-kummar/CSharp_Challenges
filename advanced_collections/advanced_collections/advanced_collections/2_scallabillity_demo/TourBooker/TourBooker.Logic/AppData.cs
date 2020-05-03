6bf037dc36f1feaff411c8095b5f3a3849346331
Naveen Kummar <r.naveenkummar@gmail.com>
2020-05-03 10:02:57 +0530

    Use Disctinory for lookup
    Use Dictinory's TryGetValue for lookup and extension method to convert List to Dictinory which use LINQ internally

--- advanced_collections/advanced_collections/advanced_collections/2_scallabillity_demo/TourBooker/TourBooker.Logic/AppData.cs
+++ advanced_collections/advanced_collections/advanced_collections/2_scallabillity_demo/TourBooker/TourBooker.Logic/AppData.cs
@@ -9,15 +9,18 @@ namespace Pluralsight.AdvCShColls.TourBooker.Logic
 	public class AppData
 	{
 		public List<Country> AllCountries { get; private set; }
-		public void Initialize(string csvFilePath)
+        public Dictionary<string, Country> AllCountriesByKey { get; private set; }
+        public void Initialize(string csvFilePath)
 		{
 			CsvReader reader = new CsvReader(csvFilePath);
 			this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
 
-			// during the module, the above line is changed to
-			//this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
+            this.AllCountriesByKey = this.AllCountries.ToDictionary(x=>x.Code);
+
+            // during the module, the above line is changed to
+            //this.AllCountries = reader.ReadAllCountries().OrderBy(x=>x.Name).ToList();
 
 
-		}
+        }
 	}
 }
