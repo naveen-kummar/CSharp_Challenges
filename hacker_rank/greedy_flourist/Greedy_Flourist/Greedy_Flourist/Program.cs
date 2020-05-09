64121a01e75b8219ada8fc7a07a41a852786dbde
Naveen Kummar <r.naveenkummar@gmail.com>
2020-05-09 12:50:45 +0530

    Greedy fourist
    Resolvesd and test case passed using SortedDictinory

--- hacker_rank/greedy_flourist/Greedy_Flourist/Greedy_Flourist/Program.cs
+++ hacker_rank/greedy_flourist/Greedy_Flourist/Greedy_Flourist/Program.cs
@@ -11,7 +11,26 @@ using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using System.Text;
 using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 
+public class Sort_Descending : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if(x > y)
+        {
+            return -1;
+        }
+        else if(x < y)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
 class Solution
 {
 
@@ -21,17 +40,17 @@ class Solution
 
         int minimum_cost = 0;
         List<int> flower_cost = new List<int>(c);
+        Sort_Descending _descending = new Sort_Descending();
+        SortedDictionary<int, int> flower_cost_dict = new SortedDictionary<int, int>(flower_cost.ToDictionary(x=>x), _descending);
 
-        flower_cost.Sort();
-        flower_cost.Reverse();
 
-        int total_flowers = flower_cost.Count();
+        int total_flowers = flower_cost_dict.Count();
         int friend_iter = 1;
         for (int flower = 0; flower < total_flowers;)
         {
             for (int friend = 0; (friend < k) && (flower < total_flowers); friend++)
             {
-                minimum_cost += flower_cost[flower] * friend_iter;
+                minimum_cost += flower_cost_dict.ElementAt(flower).Key * friend_iter;
                 ++flower;
             }
 
